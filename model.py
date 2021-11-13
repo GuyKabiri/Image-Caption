7b640a282fc3b961352bcb4e3e51ec5a77c7844a
Guy Kabiri <guy.kabiri@s.afeka.ac.il>
2021-11-13 18:58:08 +0200

    change model backbone to resnet

--- model.py
+++ model.py
@@ -7,18 +7,26 @@ from torch.functional import F
 
 
 class Encoder(nn.Module):
-    def __init__(self, embed_size, train_backbone=False) -> None:
+    def __init__(self, embed_size, train_backbone=False, drop_prob=0.3) -> None:
         super(Encoder, self).__init__()
         self.train_backbone = train_backbone
-        self.model = models.inception_v3(pretrained=True, aux_logits=False)
-        self.model.fc = nn.Linear(self.model.fc.in_features, embed_size)
+        resnet = models.resnet50(pretrained=True)
+
+        modules = list(resnet.children())[:-1]
+        self.model = nn.Sequential(*modules)
+        
+        self.fc = nn.Linear(resnet.fc.in_features, embed_size)
         self.dropout = nn.Dropout(0.3)
-        self.relu = nn.ReLU()
         self.freeze()
+
+        # self.dropout = nn.Dropout(0.3)
+        # self.relu = nn.ReLU()
 
     def forward(self, images):
         output = self.model(images)
-        output = self.dropout(self.relu(output))
+        output = output.view(output.size(0), -1)
+        output = self.dropout(output)
+        output = self.fc(output)
         return output
 
     def freeze(self):
@@ -31,47 +39,72 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers) -> None:
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers, drop_prob=0.3) -> None:
         super(Decoder, self).__init__()
-        self.embed = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size)
-        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_size, vocab_size)
-        self.dropout = nn.Dropout(0.3)
+        self.dropout = nn.Dropout(drop_prob)
 
     def forward(self, features, captions):
-        embeddings = self.dropout(self.embed(captions))
-        embeddings = torch.cat((features.unsqueeze(0), embeddings), dim=0)
-        hiddens, _ = self.lstm(embeddings)
-        outputs = self.linear(hiddens)
-        return outputs
+        print(features.shape, captions.shape)
+        embeds = self.embed(captions[:-1])
+        print(features.unsqueeze(0).shape, embeds.shape)
+        x = torch.cat((features.unsqueeze(0), embeds), dim=0) 
+        x, _ = self.lstm(x)
+        x = self.dropout(x)
+        x = self.linear(x)
+        return x
+
 
 
 class EncoderDecoder(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers, train_backbone=False) -> None:
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers, train_backbone=False, drop_prob=0.3) -> None:
         super(EncoderDecoder, self).__init__()
-        self.encoder = Encoder(embed_size, train_backbone)
-        self.decoder = Decoder(embed_size, hidden_size, vocab_size, num_layers)
+        self.encoder = Encoder(embed_size, train_backbone, drop_prob)
+        self.decoder = Decoder(embed_size, hidden_size, vocab_size, num_layers, drop_prob)
 
     def forward(self, images, captions):
-        x = self.encoder(images)
-        outputs = self.decoder(x, captions)
+        outputs = self.encoder(images)
+        outputs = self.decoder(outputs, captions)
         return outputs
 
     def caption(self, image, vocabulary, max_length=50):
-        result = []
+        captions = []
+        batch_size = image.size(0)
 
-        with torch.no_grad():
-            x = self.encoder(image).unsqueeze(0)
-            states = None
+        with torch.no_grad():            
+            for i in range(max_length):
+                output, hidden = self.decoder.lstm(inputs, hidden)
+                output = self.decoder.linear(output)
+                output = output.view(batch_size, -1)
             
-            for _ in range(max_length):
-                hiddens, states = self.decoder.lstm(x, states)
-                output = self.decoder.linear(hiddens.squeeze(0))
-                predicted = output.argmax(1)
-                result.append(predicted.item())
-                x = self.decoder.embed(output).unsqueeze(0)
+                #select the word with most val
+                predicted_word_idx = output.argmax(dim=1)
                 
-                if vocabulary.itos[predicted.item()] == '<EOS>':
+                #save the generated word
+                captions.append(predicted_word_idx.item())
+                
+                #end if <EOS detected>
+                if vocabulary.itos[predicted_word_idx.item()] == '<EOS>':
                     break
                 
-        return [vocabulary.itos[idx] for  idx in result]
+                #send generated word as the next caption
+                inputs = self.decoder.embed(predicted_word_idx.unsqueeze(0))
+            
+            #covert the vocab idx to words and return sentence
+            return [vocabulary.itos[idx] for idx in captions]
+        #     x = self.encoder(image).unsqueeze(0)
+        #     states = None
+            
+        #     for _ in range(max_length):
+        #         hiddens, states = self.decoder.lstm(x, states)
+        #         output = self.decoder.linear(hiddens.squeeze(0))
+        #         predicted = output.argmax(1)
+        #         result.append(predicted.item())
+        #         x = self.decoder.embed(output).unsqueeze(0)
+                
+        #         if vocabulary.itos[predicted.item()] == '<EOS>':
+        #             break
+                
+        # return [vocabulary.itos[idx] for  idx in result]
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import torch
+from torch.utils import data
+from torch.utils.data import dataloader
 from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
@@ -12,9 +14,9 @@ from tqdm import tqdm
 
 def train():
     transform = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Resize(size=(224, 224))
-            ])
+                    transforms.ToTensor(),
+                    transforms.Resize(size=(224, 224))
+                ])
             
     train_loader = get_loader(
         root_folder="data/flickr8k/images/",
@@ -23,16 +25,18 @@ def train():
     )
     torch.backends.cudnn.benchmark = True
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
 
 
     # Hyperparameters 
     load_model = False
-    embed_size = 256
-    hidden_size = 256
+    embed_size = 400
+    hidden_size = 512
+    vocab_size =  len(train_loader.dataset.vocab)
     num_layers = 5
-    num_epochs = 30
-    learning_rate = 1e-5
-    vocab_size = len(train_loader.dataset.vocab)
+    learning_rate = 1e-4
+    num_epochs = 2
+    print_every = 100
 
 
     # Tensorboard training monitor
@@ -40,50 +44,94 @@ def train():
     step = 0
 
     # initialize model, loss, optimizer etc.
-    model = EncoderDecoder(embed_size, hidden_size, vocab_size, num_layers)
-    criterion = nn.CrossEntropyLoss(ignore_index=train_loader.dataset.vocab.stoi["<PAD>"])
+    model = EncoderDecoder(embed_size, hidden_size, vocab_size, num_layers).to(device)
+    criterion = nn.CrossEntropyLoss(ignore_index=train_loader.dataset.vocab.stoi['<PAD>'])
     optimizer = optim.Adam(model.parameters(), lr = learning_rate)
     save_model = True
 
     if load_model:
-        step = load_checkpoint(torch.load("my_checkpoint.pth.tar"), model, optimizer)
+        step = load_checkpoint(torch.load('my_checkpoint.pth.tar'), model, optimizer)
 
-    model = model.train().to(device)
-
-    for epoch in range(num_epochs):
-        # print_examples(model, device, dataset)
-
-        for idx, (imgs, captions) in tqdm(
+    model = model.train()
+    
+    for epoch in range(1, num_epochs + 1):   
+        for idx, (images, captions) in tqdm(
             enumerate(train_loader), total=len(train_loader)
         ):
-            imgs = imgs.to(device)
-            captions = captions.to(device)
-            
-            score = model(imgs, captions[:-1])
-
-            # print(score.shape, captions.shape)
-            # print(score.reshape(-1, score.shape[2]).shape, captions.reshape(-1).shape)
-
+            images, captions = images.to(device), captions.to(device)
 
             optimizer.zero_grad()
-            loss = criterion(score.reshape(-1, score.shape[2]), captions.reshape(-1))
-            # loss = criterion(score, captions)
 
-            writer.add_scalar('loss', loss.item(), step)
-            
-            step += 1
+            outputs = model(images, captions)
+            loss = criterion(outputs.view(-1, vocab_size), captions.view(-1))
+
             loss.backward()
             optimizer.step()
-            del imgs, captions, loss
+            
+            if (idx + 1) % print_every == 0:
+                print("Epoch: {} loss: {:.5f}".format(epoch, loss.item()))
+                
+                
+                # #generate the caption
+                # model.eval()
+                # with torch.no_grad():
+                #     dataiter = iter(dataloader)
+                #     img,_ = next(dataiter)
+                #     features = model.encoder(img[0:1].to(device))
+                #     caps = model.decoder.generate_caption(features.unsqueeze(0),vocab=dataset.vocab)
+                #     caption = ' '.join(caps)
+                #     print_examples(img[0], title=caption,)
+                    
+                # model.train()
+            
+            del images, captions, loss
             torch.cuda.empty_cache()
 
-        if save_model:
-            checkpoint = {
-                "state_dict": model.state_dict(),
-                "optimizer": optimizer.state_dict(),
-                "step": step,
-            }
-            save_checkpoint(checkpoint)
+
+
+
+
+
+
+
+
+
+
+
+    # for epoch in range(num_epochs):
+    #     # print_examples(model, device, dataset)
+
+    #     for idx, (imgs, captions) in tqdm(
+    #         enumerate(train_loader), total=len(train_loader)
+    #     ):
+    #         imgs = imgs.to(device)
+    #         captions = captions.to(device)
+            
+    #         score = model(imgs, captions[:-1])
+
+    #         # print(score.shape, captions.shape)
+    #         # print(score.reshape(-1, score.shape[2]).shape, captions.reshape(-1).shape)
+
+
+    #         optimizer.zero_grad()
+    #         loss = criterion(score.reshape(-1, score.shape[2]), captions.reshape(-1))
+    #         # loss = criterion(score, captions)
+
+    #         writer.add_scalar('loss', loss.item(), step)
+            
+    #         step += 1
+    #         loss.backward()
+    #         optimizer.step()
+    #         del imgs, captions, loss
+    #         torch.cuda.empty_cache()
+
+    #     if save_model:
+    #         checkpoint = {
+    #             "state_dict": model.state_dict(),
+    #             "optimizer": optimizer.state_dict(),
+    #             "step": step,
+    #         }
+    #         save_checkpoint(checkpoint)
 
 
 if __name__ == "__main__":
