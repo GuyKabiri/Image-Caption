aa1df6fa2f24e0c70f17f721b80eb3dca982448e
Guy Kabiri <guy.kabiri@s.afeka.ac.il>
2021-11-26 13:13:01 +0200

    add explanation and test examples to notebook

--- config.py
+++ config.py
@@ -2,30 +2,30 @@ import torch.nn as nn
 import torch.optim as optim
 
 class CFG:
-    model_name = 'resnet152'
+    seed = 42
+    model_name = 'inceptionv3'
     model_path = None
     load_model = False
     embed_size = 512
     hidden_size = 512
     vocab_size =  2994
     lstm_num_layers = 2
+    batch_size = 32
     learning_rate = 1e-3
     num_epochs = 150
     drop_rate = 0.5
+    train_size = 0.75
     pretrained = True
     train_backbone = False
 
     criterion = nn.CrossEntropyLoss
+    criterion_dict = { }
 
     optimizer = optim.Adam
     optimizer_dict = {  'lr': learning_rate, }
 
-    scheduler = None
-    scheduler_dict = None
-    
-    # scheduler = optim.lr_scheduler.StepLR
-    # scheduler_dict = { 'step_size': 5,
-    #                     'gamma':    0.1 }
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau
+    scheduler_dict = { 'factor': 0.8, 'patience': 2 }
 
     def save(path):
         save_path = path + '/model_dict.json'
