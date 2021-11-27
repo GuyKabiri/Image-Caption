77250a7ae6e0139bf3367425ae05219c3569d7f4
Guy Kabiri <guy.kabiri@s.afeka.ac.il>
2021-11-27 10:29:01 +0200

    last configuration

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ import torch.optim as optim
 class CFG:
     seed = 42
     model_name = 'inceptionv3'
-    model_path = None
-    load_model = False
+    model_path = 'runs/26-11-21_20:20/models/model_checkpoint.pth'
+    load_model = True
     embed_size = 512
     hidden_size = 512
     vocab_size =  2994
-    lstm_num_layers = 2
-    batch_size = 32
-    learning_rate = 1e-3
-    num_epochs = 150
-    drop_rate = 0.5
+    lstm_num_layers = 3
+    batch_size = 64
+    learning_rate = 1e-5
+    num_epochs = 50
+    drop_rate = 0.6
     train_size = 0.75
     pretrained = True
     train_backbone = False
